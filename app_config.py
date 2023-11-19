d9ccf0edeec72d378cb4eb470489d5a73cea9715
Stefano <stef.pro@hotmail.com>
2023-11-18 22:39:49 -0500

    chore: updated app_config.py file

--- app_config.py
+++ app_config.py
@@ -1,4 +1,4 @@
-CONST_MONGO_URL = "mongodb+srv://stefpro96:lCM3T1QJmpJl9YPI@cluster0.kxjkdkx.mongodb.net/?retryWrites=true&w=majority"
+CONST_MONGO_URL = "<<ADD YOUR OWN MONGO URL>>"
 CONST_DATABASE_NAME = "test"
 CONST_USER_COLLECTION = "users"
 CONST_VERB_COLLECTION = "verbs"
